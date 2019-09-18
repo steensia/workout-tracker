a8a866d86acc9a7f2e3f87308f53131e25060fa0
Steen Sia <steen.sia@selecthealth.org>
2019-09-18 12:24:22 -0600

    Finished Structural Components Module

--- workout-tracker/src/app/admin/admin.component.ts
+++ workout-tracker/src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  public images = [
+    { src: 'assets/images/bike-1.png', title: 'Bike 1' },
+    { src: 'assets/images/row-1.png', title: 'Row 1' },
+    { src: 'assets/images/row-2.png', title: 'Row 2' },
+    { src: 'assets/images/run-1.png', title: 'Run 1' }
+  ];
+  @ViewChild('imgCarousel', {static: false}) imgCarousel: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  beforeChange($event){
+    console.log('before tab change', $event);
+    if ($event.nextId === 'tabImages'){
+      //$event.preventDefault();
+    }
+  }
+
+  prev() {
+    this.imgCarousel.prev();
+  }
+
+  next(){
+    this.imgCarousel.next();
+  }
 }
